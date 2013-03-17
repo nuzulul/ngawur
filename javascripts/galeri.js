99c8ba8bd63833e18168bba4fd8c7ad9e101cb2f
nuzulul <nuzulz@gmail.com>
2013-03-17 17:00:44 +0700

    Create galeri.js

---
+++ javascripts/galeri.js
@@ -0,0 +1,3 @@
+document.write("<div id='wrapper'><div id='content'>");
+document.write("<div id='fb_gallery' app_id='109267179132135' page_id='nuzululzulkarnainhaq'></div>");
+document.write("</div></div>");
