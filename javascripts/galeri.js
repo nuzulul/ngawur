816556aa53880556c8622fbcfafade1279f7fca4
nuzulul <nuzulz@gmail.com>
2013-03-17 17:18:41 +0700

    Update galeri.js

--- javascripts/galeri.js
+++ javascripts/galeri.js
@@ -1,3 +1,4 @@
+document.write("<link rel='stylesheet' type='text/css' href='http://nuzulul.github.com/ngawur/stylesheets/jquery.lightbox-0.5.css' media='screen' />");
 document.write("<div id='wrapper'><div id='content'>");
 document.write("<div id='fb_gallery' app_id='109267179132135' page_id='nuzululzulkarnainhaq'></div>");
 document.write("</div></div>");
