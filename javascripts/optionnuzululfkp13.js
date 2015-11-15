159ee3e18dcbd6c919e9046c3408d9fec989b3bd
nuzulul <gpssuper2@hotmail.com>
2015-11-15 19:32:40 +0700

    Update optionnuzululfkp13.js

--- javascripts/optionnuzululfkp13.js
+++ javascripts/optionnuzululfkp13.js
@@ -2,12 +2,4 @@ var elegan = '<div class="templatemo_box">	 			<a href="http://goo.gl/uyiYiR"><h
 document.getElementsByClassName('side_menu')[0].outerHTML = '<ul class="side_menu"><li><a href="kategori_isi-58356-Artikel.html">Artikel</a></li><li><a href="kategori_isi-58359-Berita.html">Berita</a></li><li><a href="kategori_isi-58358-Catatan.html">Catatan</a></li><li><a href="kategori_isi-58360-Download.html">Download</a></li><li><a href="kategori_isi-58361-Galeri.html">Galeri</a></li><li><a href="kategori_isi-52060-Ngawur.html">Ngawur</a></li><li><a href="kategori_isi-58352-Quotes.html">Quotes </a></li><li><a href="kategori_isi-58357-Tutorial.html">Tutorial</a></li></ul>';
 document.getElementById('templatemo_content').innerHTML = elegan+document.getElementById('templatemo_content').innerHTML;
 
-function fbpop()
-{
-document.getElementById("fbpop").innerHTML = '<div id="fbpopdisplay" style="display:none"><div class="popup-wrapper" id="popup"><div class="popup-container"><div style="height:0px;position:fixed;top:50px;color:#fff;"><h3>Welcome ! Please Suport Me :)</h3></div> <iframe src="http://nuzulul.github.io/ngawur/pagenuzulhook.html" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:340px; height:290px;" allowTransparency="true"></iframe> <a class="popup-close" href="#popup">X</a> </div> </div></div>';
-}
 
-addLoadEvent(fbpop);
-function fbpopdisplay(){document.getElementById('fbpopdisplay').style.display='block';}
-function fbpoptime() {setTimeout(function () { fbpopdisplay(); }, 8000);}
-addLoadEvent(fbpoptime);
