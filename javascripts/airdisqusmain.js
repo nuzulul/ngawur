299a2bce0ce8a212e1fb72b8f47b56dca675e77e
nuzulul <gpssuper2@hotmail.com>
2015-11-16 05:01:31 +0700

    Create airdisqusmain.js

---
+++ javascripts/airdisqusmain.js
@@ -0,0 +1,31 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+function loadscript()
+{
+document.getElementsByName('form3')[0].innerHTML = '<div id="disqus_thread"></div><br><a title="Pasang Disqus Comment di Blog Unair" target="_blank" href="http://goo.gl/H3MU3E">Pasang Disqus Comment di Blog Unair</a>';
+
+    /* * * CONFIGURATION VARIABLES * * */
+    //var disqus_shortname = 'blogunair';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+}
+
+function airdisqustime() {setTimeout(function () { loadscript(); }, 100);}
+addLoadEvent(airdisqustime);
