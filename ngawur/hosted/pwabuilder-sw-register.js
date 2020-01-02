4fbc65dc24c95854a74b86569bdee5846106d030
nuzulul <nuzulz@gmail.com>
2020-01-02 20:21:41 +0700

    s

--- ngawur/hosted/pwabuilder-sw-register.js
+++ ngawur/hosted/pwabuilder-sw-register.js
@@ -1,4 +1,4 @@
-// This is the service worker with the Advanced caching
+// This is the "Offline page" service worker
 
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
