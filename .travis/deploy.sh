9de6113b534b56caffec07e2c3dd02f1e153ab5f
nuzulul <nuzulz@gmail.com>
2020-11-13 18:09:38 +0700

    tes5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,23 +3,17 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git
+git clone https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git --branch site _site
 
 # build with Jekyll into "_site"
 bundle exec jekyll build
 
+cd _site
+
 git config user.name "${GITHUB_NAME}"
 git config user.email "${GITHUB_EMAIL}"
 
-git stash
-git checkout site
-git pull origin site
-
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.travis.yml' -exec rm -rf {} \;
-mv _site/* .
-rm -R _site/
-
-git add -fA
+git add -all
 git commit --allow-empty -m "[ci skip] [skip ci] Page release #$TRAVIS_BUILD_NUMBER"
 git push -q --force https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git HEAD:site
 
