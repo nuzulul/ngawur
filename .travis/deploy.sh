4d7866e1b5e3a16b8eb958113c27324083d6aab1
nuzulul <nuzulz@gmail.com>
2020-11-13 17:35:53 +0700

    tes4

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,13 @@
+# cleanup "_site"
+rm -rf _site
+mkdir _site
+
+# clone remote repo to "_site"
+git clone https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git
+
+# build with Jekyll into "_site"
+bundle exec jekyll build
+
 git config user.name "${GITHUB_NAME}"
 git config user.email "${GITHUB_EMAIL}"
 
@@ -5,7 +15,7 @@ git stash
 git checkout site
 git pull origin site
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.travis' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.travis.yml' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
