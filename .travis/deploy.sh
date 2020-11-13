ad2adaa60f0dda2a0d3b8c62dc14a42e55609a07
nuzulul <nuzulz@gmail.com>
2020-11-14 00:34:58 +0700

    test7

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,9 @@ mkdir _site
 
 # clone remote repo to "_site"
 git clone https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git --branch site _site
+cd _site
+git rm -rf .
+cd ..
 
 # build with Jekyll into "_site"
 bundle exec jekyll build
@@ -14,7 +17,7 @@ git config user.name "${GITHUB_NAME}"
 git config user.email "${GITHUB_EMAIL}"
 
 git add --all
-git commit --allow-empty -m "[ci skip] [skip ci] Page release #$TRAVIS_BUILD_NUMBER"
+git commit --allow-empty -m "Page release #$TRAVIS_BUILD_NUMBER [ci skip] [skip ci]"
 git push -q --force https://${GITHUB_TOKEN}@github.com/nuzulul/ngawur.git HEAD:site
 
 echo "deployed successfully"
--- _posts/2020-11-12-testing-lagi.md
+++ _posts/2020-11-12-testing-lagi.md
@@ -5,4 +5,4 @@ date: 2020-11-12T13:12:26.111Z
 modified: 2020-11-12T13:12:26.215Z
 comments: true
 ---
-tes build
+tes build 7
